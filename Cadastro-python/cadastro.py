41e34d6f9ad1a464773b6bf33d9b3b11327ba014
Marinhodm <marinhodaniel235@gmail.com>
2026-08-24 14:07:04 -0400

    Add files via upload

---
+++ Cadastro-python/cadastro.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+
+print("\n--- CADASTRO ---")
+print("Nome:", nome)
+print("Idade:", idade)
+print("Cadastro realizado com sucesso!")
